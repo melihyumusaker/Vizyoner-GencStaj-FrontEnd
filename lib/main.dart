c90e4951b2c01c5f02083ffb623e1e5153625f52
Emine Betul Erdogan <betulerdgn52@gmail.com>
2023-07-25 15:17:56 +0300

    bottom nav bar, home page, yayınla page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:proje/pages/auth/login/login.dart';
 import 'package:proje/pages/hakkimizda/hakkimizda.dart';
-import 'package:proje/pages/hakkimizda/misyon.dart';
-import 'package:proje/pages/hakkimizda/vizyon.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
